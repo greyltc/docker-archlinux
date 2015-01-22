5bf567945f48802c501189a270292e341d5411a0
Grey Christoforo <grey@christoforo.net>
2015-01-22 10:41:30 -0800

    update readme and file system archive to 2014-01-22
    Former-commit-id: c372be442401f1eea830043e3e76f5cd18a896c4

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 # Arch Linux base docker container
-# Generated on Mon Jan  5 21:32:45 CET 2015
+# Generated on Thu Jan 22 10:36:16 PST 2015
+# Read the following to learn how the root filesystem image was generated:
+# https://github.com/l3iggs/docker-archlinux-builder/blob/master/README.md
 FROM scratch
 MAINTAINER l3iggs <l3iggs@live.com>
 ADD archlinux.tar.xz /
---
+++ buildme.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cat > Dockerfile << EOF
+# Arch Linux base docker container
+# Generated on `date`
+# Read the following to learn how the root filesystem image was generated:
+# https://github.com/l3iggs/docker-archlinux-builder/blob/master/README.md
+FROM scratch
+MAINTAINER l3iggs <l3iggs@live.com>
+ADD archlinux.tar.xz /
+EOF
+
+curl https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage-arch.sh > mkimage-arch.sh
+chmod +x mkimage-arch.sh
+
+#sed -i 's,tar --numeric-owner --xattrs --acls -C $ROOTFS -c . | docker import - archlinux,printf '\''n archlinux-%02d.tar\\n'\'' {2..100} | tar --numeric-owner --xattrs --acls -C $ROOTFS -c . -L 100M -f archlinux-01.tar 2>/dev/null,g' mkimage-arch.sh
+
+sed -i 's/| docker import - archlinux/-af archlinux.tar.xz/g' mkimage-arch.sh
+sed -i '/docker run -i -t archlinux echo Success./d' mkimage-arch.sh
+
+curl https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage-arch-pacman.conf > mkimage-arch-pacman.conf
+
+su -c './mkimage-arch.sh'
+
+rm mkimage-arch.sh
+rm mkimage-arch-pacman.conf
