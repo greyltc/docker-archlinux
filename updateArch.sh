afc26e5cf4b3565726360b98378286715fdf3d09
Greyson Christoforo <grey@christoforo.net>
2016-01-08 16:13:33 +0000

    stop doing meaningless things
    Former-commit-id: f08322d6b7535a0ebc1d31aaf1c29f393940c7c7

--- updateArch.sh
+++ updateArch.sh
@@ -3,12 +3,6 @@
 # install reflector
 pacman -S --noconfirm --needed reflector
 
-# backup the old mirrorlist
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-
-# get the latest mirrorlist
-curl https://www.archlinux.org/mirrorlist/all/ > /etc/pacman.d/mirrorlist
-
 # use reflector to rank mirrors by speed
 reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
 
