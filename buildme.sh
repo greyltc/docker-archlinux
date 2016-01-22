7faf696f95754e1de750964369a171c52dc9f7ad
Grey Christoforo <grey@christoforo.net>
2016-01-22 20:30:05 +0000

    roll back
    Former-commit-id: 188c7b226334e13b42808c6ccf025a50df3df0a2

--- buildme.sh
+++ buildme.sh
@@ -15,12 +15,11 @@ MAINTAINER Grey Christoforo <grey@christoforo.net>
 ADD archlinux.tar.xz /
 
 # update mirrorlist and packages
-ADD updateArch.sh /usr/bin/updateArch.sh
-RUN /usr/bin/updateArch.sh
 RUN date
 RUN ls -alh /usr/bin
 RUN ls -alh /bin
 RUN file /usr/bin/bash
+RUN echo /usr/bin/content.sh
 RUN ["/usr/bin/bash", "/usr/bin/gettext.sh", "--help"]
 RUN file /usr/bin/updateArch.sh
 RUN ["/usr/bin/bash", "/usr/bin/updateArch.sh"]
@@ -33,14 +32,10 @@ curl https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage-arch
 chmod +x /tmp/mkimage-arch.sh
 
 # install a script that will be used to update the mirror list at build time
-sed -i 's,chmod 755 $ROOTFS,chmod 755 $ROOTFS\ninstall -m755 -D /tmp/updateArch.sh -t $ROOTFS/usr/bin; cat $ROOTFS/usr/bin/updateArch.sh,g' /tmp/mkimage-arch.sh
-
-# install a script that will be used to update the mirror list at build time
-sed -i ',arch-chroot $ROOTFS /bin/sh -c '\''echo $PACMAN_MIRRORLIST > /etc/pacman.d/mirrorlist'\'',d' /tmp/mkimage-arch.sh
-#sed -i 's,arch-chroot $ROOTFS /bin/sh -c '\''echo $PACMAN_MIRRORLIST > /etc/pacman.d/mirrorlist'\'',install -m755 -D /tmp/updateArch.sh -t "$ROOTFS/usr/bin"; arch-chroot $ROOTFS /bin/sh -c '\''touch /usr/bin/updateArch.sh; sync; echo "content" > /usr/bin/content.sh'\'',g' /tmp/mkimage-arch.sh
+sed -i 's,arch-chroot $ROOTFS /bin/sh -c '\''echo $PACMAN_MIRRORLIST > /etc/pacman.d/mirrorlist'\'',install -m755 -D /tmp/updateArch.sh -t "$ROOTFS/usr/bin"; arch-chroot $ROOTFS /bin/sh -c '\''touch /usr/bin/updateArch.sh; sync; echo "content" > /usr/bin/content.sh'\'',g' /tmp/mkimage-arch.sh
 
 # instead of importing the image we'll dump the newly created image into a file: /tmp/archlinux.tar.xz
-sed -i 's,tar --numeric-owner --xattrs --acls -C $ROOTFS -c . | docker import - $DOCKER_IMAGE_NAME,cd $ROOTFS;XZ_OPT="-9 -T 0" tar -v --numeric-owner --xattrs --acls -Jcf /tmp/archlinux.tar.xz *,g' /tmp/mkimage-arch.sh
+sed -i 's,tar --numeric-owner --xattrs --acls -C $ROOTFS -c . | docker import - $DOCKER_IMAGE_NAME,cd $ROOTFS;XZ_OPT="-9e -T 0" tar --numeric-owner --xattrs --acls -Jcf /tmp/archlinux.tar.xz *,g' /tmp/mkimage-arch.sh
 
 # remove this line since it makes no sense now
 sed -i '/docker run --rm -t $DOCKER_IMAGE_NAME echo Success./d' /tmp/mkimage-arch.sh
