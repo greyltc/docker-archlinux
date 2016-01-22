9e142381b3a81c6bb6fb46ab36255683b3a97cf3
Grey Christoforo <grey@christoforo.net>
2016-01-22 17:44:49 +0000

    different addition order
    Former-commit-id: 4db25516028614329acd22e5052f124ecc002fec

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Arch Linux baseline docker container
-# Generated on Fri Jan 22 16:30:49 GMT 2016
+# Generated on Fri Jan 22 17:42:38 GMT 2016
 # Read the following to learn how the root filesystem image was generated:
 # https://github.com/greyltc/docker-archlinux/blob/master/README.md
 FROM scratch
@@ -9,10 +9,13 @@ MAINTAINER Grey Christoforo <grey@christoforo.net>
 ADD archlinux.tar.xz /
 
 # update mirrorlist and packages
+ADD updateArch.sh /usr/bin/updateArch.sh
+RUN /usr/bin/updateArch.sh
 RUN date
 RUN ls -alh /usr/bin
 RUN ls -alh /bin
 RUN file /usr/bin/bash
+RUN touch /usr/bin/content.sh
 RUN cat /usr/bin/content.sh
 RUN ["/usr/bin/bash", "/usr/bin/gettext.sh", "--help"]
 RUN file /usr/bin/updateArch.sh
--- buildme.sh
+++ buildme.sh
@@ -15,10 +15,13 @@ MAINTAINER Grey Christoforo <grey@christoforo.net>
 ADD archlinux.tar.xz /
 
 # update mirrorlist and packages
+ADD updateArch.sh /usr/bin/updateArch.sh
+RUN /usr/bin/updateArch.sh
 RUN date
 RUN ls -alh /usr/bin
 RUN ls -alh /bin
 RUN file /usr/bin/bash
+RUN touch /usr/bin/content.sh
 RUN cat /usr/bin/content.sh
 RUN ["/usr/bin/bash", "/usr/bin/gettext.sh", "--help"]
 RUN file /usr/bin/updateArch.sh
@@ -32,10 +35,14 @@ curl https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage-arch
 chmod +x /tmp/mkimage-arch.sh
 
 # install a script that will be used to update the mirror list at build time
-sed -i 's,arch-chroot $ROOTFS /bin/sh -c '\''echo $PACMAN_MIRRORLIST > /etc/pacman.d/mirrorlist'\'',install -m755 -D /tmp/updateArch.sh -t "$ROOTFS/usr/bin"; arch-chroot $ROOTFS /bin/sh -c '\''touch /usr/bin/updateArch.sh; sync; echo "content" > /usr/bin/content.sh'\'',g' /tmp/mkimage-arch.sh
+sed -i 's,chmod 755 $ROOTFS,chmod 755 $ROOTFS\ninstall -m755 -D /tmp/updateArch.sh -t $ROOTFS/usr/bin; cat $ROOTFS/usr/bin/updateArch.sh,g' /tmp/mkimage-arch.sh
+
+# install a script that will be used to update the mirror list at build time
+sed -i ',arch-chroot $ROOTFS /bin/sh -c '\''echo $PACMAN_MIRRORLIST > /etc/pacman.d/mirrorlist'\'',d' /tmp/mkimage-arch.sh
+#sed -i 's,arch-chroot $ROOTFS /bin/sh -c '\''echo $PACMAN_MIRRORLIST > /etc/pacman.d/mirrorlist'\'',install -m755 -D /tmp/updateArch.sh -t "$ROOTFS/usr/bin"; arch-chroot $ROOTFS /bin/sh -c '\''touch /usr/bin/updateArch.sh; sync; echo "content" > /usr/bin/content.sh'\'',g' /tmp/mkimage-arch.sh
 
 # instead of importing the image we'll dump the newly created image into a file: /tmp/archlinux.tar.xz
-sed -i 's,tar --numeric-owner --xattrs --acls -C $ROOTFS -c . | docker import - $DOCKER_IMAGE_NAME,cd $ROOTFS;XZ_OPT="-9 -T 0" tar --numeric-owner --xattrs --acls -Jcf /tmp/archlinux.tar.xz *,g' /tmp/mkimage-arch.sh
+sed -i 's,tar --numeric-owner --xattrs --acls -C $ROOTFS -c . | docker import - $DOCKER_IMAGE_NAME,cd $ROOTFS;XZ_OPT="-9 -T 0" tar -v --numeric-owner --xattrs --acls -Jcf /tmp/archlinux.tar.xz *,g' /tmp/mkimage-arch.sh
 
 # remove this line since it makes no sense now
 sed -i '/docker run --rm -t $DOCKER_IMAGE_NAME echo Success./d' /tmp/mkimage-arch.sh
