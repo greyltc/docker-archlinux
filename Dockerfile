5afb9f759aaa5dcd7cd72aca3a4c6b175f91bb08
Grey Christoforo <grey@christoforo.net>
2016-01-24 21:14:51 +0000

    no longer needs: A) root permissions and B) to be run from an Arch host updated root fs generation based off of this project: https://github.com/tokland/arch-bootstrap
    Former-commit-id: f1286bf5fe83d1d0578a9c4f20979812c68d7fb3

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,11 @@
 # Arch Linux baseline docker container
-# Generated on Fri Jan 22 20:54:35 GMT 2016
-# Read the following to learn how the root filesystem image was generated:
-# https://github.com/greyltc/docker-archlinux/blob/master/README.md
+# Generated on Sun Jan 24 21:10:29 GMT 2016 from this specific GutHub repo and commit:
+# https://github.com/greyltc/docker-archlinux/tree/db153677a2d177b91f6cc590ec9f208cbde6c5bb
 FROM scratch
 MAINTAINER Grey Christoforo <grey@christoforo.net>
 
-# copy in the previously generated file system
+# copy in root filesystem archive
 ADD archlinux.tar.xz /
 
-# update mirrorlist and packages
-RUN date
-RUN ls -alh /usr/bin
-RUN ls -alh /bin
-RUN file /usr/bin/bash
-RUN echo /usr/bin/content.sh
-RUN ["/usr/bin/bash", "/usr/bin/gettext.sh", "--help"]
-RUN file /usr/bin/updateArch.sh
-RUN ["/usr/bin/bash", "/usr/bin/updateArch.sh"]
-RUN /usr/bin/updateArch.sh
-RUN updateArch.sh
+# perform initial container setup tasks
+RUN setup-arch-docker-container
--- buildme.sh
+++ buildme.sh
@@ -1,65 +1,42 @@
 #!/usr/bin/env bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
-cp -a "$DIR/updateArch.sh" /tmp/.
+set -e
 
-cat > Dockerfile << EOF
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
+
+cat > "$DIR/Dockerfile" << EOF
 # Arch Linux baseline docker container
-# Generated on `date`
-# Read the following to learn how the root filesystem image was generated:
-# https://github.com/greyltc/docker-archlinux/blob/master/README.md
+# Generated on `date` from this specific GutHub repo and commit:
+# https://github.com/greyltc/docker-archlinux/tree/`git rev-parse HEAD`
 FROM scratch
 MAINTAINER Grey Christoforo <grey@christoforo.net>
 
-# copy in the previously generated file system
+# copy in root filesystem archive
 ADD archlinux.tar.xz /
 
-# update mirrorlist and packages
-RUN date
-RUN ls -alh /usr/bin
-RUN ls -alh /bin
-RUN file /usr/bin/bash
-RUN echo /usr/bin/content.sh
-RUN ["/usr/bin/bash", "/usr/bin/gettext.sh", "--help"]
-RUN file /usr/bin/updateArch.sh
-RUN ["/usr/bin/bash", "/usr/bin/updateArch.sh"]
-RUN /usr/bin/updateArch.sh
-RUN updateArch.sh
+# perform initial container setup tasks
+RUN setup-arch-docker-container
 EOF
 
-# fetch the official Arch Linux generation script from Docker's github account
-curl https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage-arch.sh > /tmp/mkimage-arch.sh
-chmod +x /tmp/mkimage-arch.sh
+# make the root filesystem
+TEMP_ROOT=./tmproot
+rm -rf $TEMP_ROOT
+mkdir -p $TEMP_ROOT
+echo -e "\033[1mGenerating Arch Linux root filesystem...\033[0m"
+fakechroot fakeroot "$DIR/arch-bootstrap.sh" -a x86_64 $TEMP_ROOT
+echo -e "\033[1mRoot filesystem generation complete.\033[0m"
 
-# install a script that will be used to update the mirror list at build time
-sed -i 's,arch-chroot $ROOTFS /bin/sh -c '\''echo $PACMAN_MIRRORLIST > /etc/pacman.d/mirrorlist'\'',install -m755 -D /tmp/updateArch.sh -t "$ROOTFS/usr/bin"; arch-chroot $ROOTFS /bin/sh -c '\''touch /usr/bin/updateArch.sh; sync; echo "content" > /usr/bin/content.sh'\'',g' /tmp/mkimage-arch.sh
+# install our setup script
+echo -e "\033[1mInstalling setup script.\033[0m"
+install -m755 -D "$DIR/setup-arch-docker-container.sh" "$TEMP_ROOT/usr/bin/setup-arch-docker-container"
 
-# instead of importing the image we'll dump the newly created image into a file: /tmp/archlinux.tar.xz
-sed -i 's,tar --numeric-owner --xattrs --acls -C $ROOTFS -c . | docker import - $DOCKER_IMAGE_NAME,cd $ROOTFS;XZ_OPT="-9e -T 0" tar --numeric-owner --xattrs --acls -Jcf /tmp/archlinux.tar.xz *,g' /tmp/mkimage-arch.sh
+# make the root filesystem archive
+rm -rf archlinux.tar.xz
+pushd $TEMP_ROOT
+echo -e "\033[1mCompressing root filesystem archive...\033[0m"
+XZ_OPT="-9 -T 0" tar --owner=0 --group=0 --xattrs --acls -Jcf ../archlinux.tar.xz *
+popd
+echo -e "\033[1mRoot fs archive generation complete.\033[0m"
 
-# remove this line since it makes no sense now
-sed -i '/docker run --rm -t $DOCKER_IMAGE_NAME echo Success./d' /tmp/mkimage-arch.sh
-
-# change the ownership of the new image
-sed -i '$a chown '${USER}' /tmp/archlinux.tar.xz' /tmp/mkimage-arch.sh
-
-# fetch the fixed pacman.conf 
-curl https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage-arch-pacman.conf > /tmp/mkimage-arch-pacman.conf
-
-cd /tmp
-echo "Building Arch Linux-docker root filesystem archive."
-echo -e "\033[1msudo is required for the arch-chroot command.\033[0m"
-sudo /tmp/mkimage-arch.sh
-
-if [ -f /tmp/archlinux.tar.xz ]; then
-    echo "Arch Linux-docker root filesystem archive build complete!"
-    cp /tmp/archlinux.tar.xz ${DIR}/
-    
-else
-    echo "The Arch Linux-docker root filesystem archive build failed."
-fi
-
-rm -rf /tmp/mkimage-arch.sh
-rm -rf /tmp/mkimage-arch-pacman.conf
-rm -rf /tmp/archlinux.tar.xz
+rm -rf rm -rf $TEMP_ROOT
 
