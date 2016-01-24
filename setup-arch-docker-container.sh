b75dc3f9fa6830b0644ae362d7184df4a669da6a
Grey Christoforo <grey@christoforo.net>
2016-01-24 21:42:38 +0000

    added some comments to the setup script
    Former-commit-id: 714c0d0aa02ad434f19b17e203143e1bf090b8f3 [formerly e18b1e09d9f377b09362e8d31b1a17c40a922fc1]
    Former-commit-id: 29d7b883a98c3785892879aafc1d8e06b914e678

--- setup-arch-docker-container.sh
+++ setup-arch-docker-container.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-set -ev
+set -e
+
+# this fails in the chroot during setup so let's run it now to build the cache
 ldconfig
 
 # populate keychain
 pacman-key --init
 pacman-key --populate archlinux
-#pkill gpg-agent
 
 # these are packages from the base group that we specifically don't want in this image
+# for various reasons, taken from here: https://github.com/docker/docker/blob/master/contrib/mkimage-arch.sh
 PKGIGNORE=(
     cryptsetup
     device-mapper
@@ -42,13 +44,20 @@ PACKAGES=($(comm -13 <(printf '%s\n' "${PKGIGNORE[@]}" | LC_ALL=C sort) <(printf
 # install relevant packages from the base group
 pacman -S --noconfirm --needed "${PACKAGES[@]}"
 
-# stuff that needs comments
+# set the timezone
 ln -s /usr/share/zoneinfo/UTC /etc/localtime
+
+# set the locale
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
+
+# use reflector to rank the fastest mirrors
 pacman -S --noconfirm --needed reflector
 reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Rs reflector --noconfirm
+
+# update all packages and cache
 pacman -Syyu --noconfirm --needed
+
+# remove all cached package archives
 paccache -r -k0
-ls /var/cache/pacman/pkg
