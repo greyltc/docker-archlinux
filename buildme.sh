eb790492df97c5f180a79e831c47224ca2afc8ff
Grey Christoforo <grey@christoforo.net>
2016-03-01 16:02:16 +0000

    This install takes place during the build now
    Former-commit-id: fb35b04465388c273c1504089559184535e45b0c
    Former-commit-id: 4dca1364d6739a0736c26d8c30da3e23556b604a
    Former-commit-id: 4e0fb5a5c97255d04a25dc2420e0f774bfb898a9

--- buildme.sh
+++ buildme.sh
@@ -13,11 +13,6 @@ MAINTAINER Grey Christoforo <grey@christoforo.net>
 # copy in super minimal root filesystem archive
 ADD archlinux.tar.xz /
 
-# properly reinstall the bare minimum packages required for pacman, plus the filesystem package
-# this list can be generated under Arch Linux by running:
-# bash <(curl -L 'https://raw.githubusercontent.com/greyltc/arch-bootstrap/master/get-pacman-dependencies.sh')
-#RUN ["/usr/bin/pacman", "--noconfirm", "-Sy", "--force", "coreutils", "bash", "grep", "gawk", "file", "tar", "sed", "acl", "archlinux-keyring", "attr", "bzip2", "curl", "e2fsprogs", "expat", "glibc", "gpgme", "keyutils", "krb5", "libarchive", "libassuan", "libgpg-error", "libidn", "libssh2", "lzo", "openssl", "pacman", "pacman-mirrorlist", "xz", "zlib", "filesystem"]
-
 # perform initial container setup tasks
 RUN setup-arch-docker-container
 
