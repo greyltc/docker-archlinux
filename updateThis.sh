cd8f148a03b038024296e0cff6e486570d6f89fe
Grey Christoforo <grey@christoforo.net>
2016-03-19 14:23:09 +0000

    add updater script
    Former-commit-id: 7db4e87f560b6c5684344ffc0f2fde0ea4b378fe

---
+++ updateThis.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e -u -o pipefail
+
+./buildme.sh &> buildme.log
+git add buildme.log
+git add archlinux.tar.xz
+git commit -m "$(date): bump to latest Arch Linux"
+git push -u origin master
+git tag -a $(date -I) -m "$(date) snapshot" && git push --tags
