a08d2cc7b2094c26d44a6ec752e671569901564b
Grey Christoforo <grey@christoforo.net>
2016-03-19 14:25:59 +0000

    include dockerfile in update!
    Former-commit-id: 8fb5bf877b29c1175b51c26b505a0e95227fa543 [formerly 5988ac879c88dbfaebdd48edebc402779c36d0be]
    Former-commit-id: 4237dcc5f773aa4717ed1e754f736ad50279293d [formerly 0609d49b03bd4606ad790eb3fca547c0bb89f7a2]
    Former-commit-id: 0ee7d3ecdb8c3551992b634d973c90d4cc7bddb7

--- updateThis.sh
+++ updateThis.sh
@@ -4,6 +4,7 @@ set -e -u -o pipefail
 ./buildme.sh &> buildme.log
 git add buildme.log
 git add archlinux.tar.xz
+git add Dockerfile
 git commit -m "$(date): bump to latest Arch Linux"
 git push -u origin master
 git tag -a $(date -I) -m "$(date) snapshot" && git push --tags
