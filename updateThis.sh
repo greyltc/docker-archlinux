c8b7fd7a321ea8444d1b071f73d7013348c03843
Grey Christoforo <grey@christoforo.net>
2018-12-05 17:46:37 +0000

    revert
    Former-commit-id: 67d8b1bf4e510d2c06aa0ac9fc395d667c4aa232 [formerly 8f710778584421e7826f5b5c9cd034834f07d88c]
    Former-commit-id: 546fc973fdeee7d0314aad04f89210a835ad89cc

--- updateThis.sh
+++ updateThis.sh
@@ -2,4 +2,9 @@
 set -e -u -o pipefail
 
 ./build_root_targz.sh |& tee thisBuild.log
-./gitPush.sh
+git add thisBuild.log
+git add archlinux.tar.xz
+git add Dockerfile
+git commit -m "$(date): bump to latest Arch Linux"
+git push -u origin master
+git tag -a $(date -I) -m "$(date) snapshot" && git push --tags
