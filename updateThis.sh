06d519376deee11ad337bebc61cde0522d7b0d68
Grey Christoforo <grey@christoforo.net>
2018-12-05 17:44:14 +0000

    split push out
    Former-commit-id: f643afa96e0d0f80a5ad77b8692370847eefe370 [formerly 6d1c545d302f8bbf34931d59631343e32e3bef81]
    Former-commit-id: 7a1edbcb6ff3cb4e62c5c5d072fc1c320b8f48d0

--- updateThis.sh
+++ updateThis.sh
@@ -2,9 +2,4 @@
 set -e -u -o pipefail
 
 ./build_root_targz.sh |& tee thisBuild.log
-git add thisBuild.log
-git add archlinux.tar.xz
-git add Dockerfile
-git commit -m "$(date): bump to latest Arch Linux"
-git push -u origin master
-git tag -a $(date -I) -m "$(date) snapshot" && git push --tags
+./gitPush.sh
