bbc33017afe31561d13cff7d623f4b65ac75ba40
Grey Christoforo <grey@christoforo.net>
2018-12-05 17:52:51 +0000

    Create travisUpdateThis.sh
    Former-commit-id: 82bc3fd96464071bf441585a48320eace24ad967 [formerly 37b9325c6d4b48a5d08f807deec63577d5061add]
    Former-commit-id: 905f97ef7721a8fa49b882cde41a1935d8794bfc [formerly 53009e465d0a63d5a5e6fab74c7425bd034282a4]
    Former-commit-id: 295c78966109ad9de2489e477fd410250c31e137

---
+++ travisUpdateThis.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e -u -o pipefail
+
+./build_root_targz.sh |& tee thisBuild.log
+
+git config user.name "Travis CI"
+git config user.email "kicad.klc.bot@gmail.com"
+
+git add thisBuild.log
+git add archlinux.tar.xz
+git add Dockerfile
+
+chmod 600 travis_key
+eval `ssh-agent -s`
+
+ssh-add travis_key
+
+git commit -m "$(date): bump to latest Arch Linux -- travis"
+
+export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
+git remote set-url --push origin git@github.com:greyltc/docker-archlinux.git
+
+TAG="$(date -u -I)-travis"
+git tag -a ${TAG} -m "$(date) snapshot" travis
+git push origin ${TAG} --force -v --progress
