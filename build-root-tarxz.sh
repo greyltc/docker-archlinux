d27d2f2cab281fb51fb887bf6a0d57b2beebbf30
Greyson Christoforo <grey@christoforo.net>
2021-12-05 10:33:29 +0000

    Update build-root-tarxz.sh
    Former-commit-id: 3cf0fce8984c0bc84e78b0d2dc31ccc14c46eeff

--- build-root-tarxz.sh
+++ build-root-tarxz.sh
@@ -3,6 +3,9 @@ set -e -u -o pipefail
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 
+# build architecture
+ARCH=$1
+
 cat > "$DIR/Dockerfile" << EOF
 # Arch Linux baseline docker container
 # Generated on `date` using code in this GitHub repo:
@@ -11,7 +14,7 @@ FROM scratch
 MAINTAINER Grey Christoforo <grey@christoforo.net>
 
 # copy in super minimal root filesystem archive
-ADD archlinux.tar.xz /
+ADD archlinux-amd64.tar.xz /
 
 # perform initial container setup tasks
 RUN setup-arch-docker-container
@@ -22,8 +25,15 @@ EOF
 
 # make the root filesystem
 echo -e "\033[1mGenerating Arch Linux root filesystem...\033[0m"
-TMP_ROOT=./tmproot
-rm -rf $TMP_ROOT || True
+
+TMP_ROOT=$(mktemp -d)
+
+# Bail out if the temp directory wasn't created successfully.
+if [ ! -e ${TMP_ROOT} ]; then
+    >&2 echo "Failed to create temp directory"
+    exit 1
+fi
+#TODO: work in ARCH here
 bash <(curl --silent --tlsv1.3 --location 'https://raw.githubusercontent.com/greyltc/arch-bootstrap/master/arch-bootstrap.sh') -s1 $TMP_ROOT
 echo -e "\033[1mRoot filesystem generation complete.\033[0m"
 
@@ -41,22 +51,24 @@ install -m755 -D "$DIR/cleanupImage.sh" "$TMP_ROOT/usr/sbin/cleanup-image"
 # dockerify the rootfs
 echo -e "\033[1mDoing Docker things to the root file system.\033[0m"
 pushd $TMP_ROOT
-DEV=dev
-rm -rf $DEV
-mkdir -p $DEV
-fakeroot mknod -m 666 $DEV/null c 1 3
-fakeroot mknod -m 666 $DEV/zero c 1 5
-fakeroot mknod -m 666 $DEV/random c 1 8
-fakeroot mknod -m 666 $DEV/urandom c 1 9
-fakeroot mkdir -m 755 $DEV/pts
-fakeroot mkdir -m 1777 $DEV/shm
-fakeroot mknod -m 666 $DEV/tty c 5 0
-fakeroot mknod -m 600 $DEV/console c 5 1
-fakeroot mknod -m 666 $DEV/tty0 c 4 0
-fakeroot mknod -m 666 $DEV/full c 1 7
-fakeroot mknod -m 600 $DEV/initctl p
-fakeroot mknod -m 666 $DEV/ptmx c 5 2
-ln -sf /proc/self/fd $DEV/fd
+rm -rf dev
+mkdir -p dev
+
+pushd dev
+fakeroot mknod -m 666 null c 1 3
+fakeroot mknod -m 666 zero c 1 5
+fakeroot mknod -m 666 random c 1 8
+fakeroot mknod -m 666 urandom c 1 9
+fakeroot mkdir -m 755 pts
+fakeroot mkdir -m 1777 shm
+fakeroot mknod -m 666 tty c 5 0
+fakeroot mknod -m 600 console c 5 1
+fakeroot mknod -m 666 tty0 c 4 0
+fakeroot mknod -m 666 full c 1 7
+fakeroot mknod -m 600 initctl p
+fakeroot mknod -m 666 ptmx c 5 2
+ln -sf /proc/self/fd fd
+popd
 
 # remove some files that we don't need here
 rm -rf usr/share/man/*
@@ -67,11 +79,11 @@ rm -rf etc/shadow*
 popd
 
 # make the root filesystem archive
-rm -rf archlinux.tar.xz
-pushd $TMP_ROOT
+rm -f archlinux-${ARCH}.tar.xz
+pushd "${TMP_ROOT}"
 echo -e "\033[1mCompressing root filesystem archive...\033[0m"
-XZ_OPT="-9e --threads=0" tar --owner=0 --group=0 --xattrs --acls -Jcf ../archlinux.tar.xz *
+XZ_OPT="-9e --threads=0" tar --owner=0 --group=0 --xattrs --acls -Jcf ../archlinux-${ARCH}.tar.xz *
 popd
 echo -e "\033[1mRoot fs archive generation complete.\033[0m"
 
-rm -rf $TMP_ROOT
+rm -r "${TMP_ROOT}"
