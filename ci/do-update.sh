4894ddc7b9a2f83d9a8e9c158bab851e09ed6d2a
Greyson Christoforo <grey@christoforo.net>
2021-12-05 10:46:54 +0000

    Update do-update.sh
    Former-commit-id: 627d2b920af48868aa5da0ad73646e7a823c1bc8

--- ci/do-update.sh
+++ ci/do-update.sh
@@ -8,7 +8,7 @@ pushd $(git rev-parse --show-toplevel)
 git add build-${ARCH}.log
 git add archlinux-${ARCH}.tar.xz
 git add Dockerfile
-git commit -m "$(date): bump to latest Arch Linux"
-git push -u origin master
-git tag -a $(date -I) -m "$(date) snapshot" && git push --tags
+#git commit -m "$(date): bump to latest Arch Linux"
+#git push -u origin master
+#git tag -a $(date -I) -m "$(date) snapshot" && git push --tags
 popd
