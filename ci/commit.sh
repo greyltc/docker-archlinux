20a3986a7b5ad7c92d8f2e2365e334c1848438c2
Greyson Christoforo <grey@christoforo.net>
2021-12-07 12:11:42 +0000

    Update commit.sh
    Former-commit-id: 7206b1767fd971911bb3461a21a479907a080345 [formerly 595dbe7506dcf4ff28fac851ef3badfd9af38d63]
    Former-commit-id: 5045608587e80a33fbc8489014e0382705da051a

--- ci/commit.sh
+++ ci/commit.sh
@@ -3,7 +3,9 @@
 TAG="${1}"
 if ! test -z "${TAG}"
 then
-  git commit -m "root tarball(s) rebuilt"
+  git config --global user.name 'CI Robot'
+  git config --global user.email 'ci@robot.fake'
+  git commit -m "version ${TAG} root tarball(s) rebuilt"
   git tag -a "${TAG}" -m "rebuilt root tarball(s)"
   git push -u origin master --tags
 fi
