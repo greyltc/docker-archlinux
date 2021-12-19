0ebb302dfdfca781438e9a3d50b38d510eaf8be9
Greyson Christoforo <grey@christoforo.net>
2021-12-19 22:12:35 +0000

    Delete ci directory
    Former-commit-id: 6c0f93cb85254b65ef1c10b0a6fb576690300ab0

--- ci/commit.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-TAG="${1}"
-if ! test -z "${TAG}"
-then
-  git commit -m "version ${TAG} root tarball(s) rebuilt"
-  git tag -a "v${TAG}" -m "rebuilt root tarball(s)"
-  git push -u origin master --tags
-fi
