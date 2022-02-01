1c3d8321f073433d25f29a978891f30e45d21c92
Greyson Christoforo <grey@christoforo.net>
2022-02-01 08:32:32 +0000

    pacman database updates

--- get-new-mirrors.sh
+++ get-new-mirrors.sh
@@ -3,7 +3,7 @@ set -e
 set -u
 
 echo "Finding the fastest Arch mirrors..."
-pacman -S pacman-contrib curl --needed --noprogressbar --noconfirm
+pacman -Syy pacman-contrib curl --needed --noprogressbar --noconfirm
 curl --silent --get --url https://archlinux.org/mirrorlist/ --data "country=all" --data "use_mirror_status=on" --data "protocol=https" > /tmp/mirrorlist
 sed 's/^#Server/Server/' --in-place /tmp/mirrorlist
 rankmirrors --verbose -n 10 --max-time 3 /tmp/mirrorlist > /tmp/fastmirrorlist
