0df846f204c4fee2341654c2907dc6d74aef5499
Greyson Christoforo <grey@christoforo.net>
2022-02-11 14:32:50 +0000

    Update get-new-mirrors.sh

--- get-new-mirrors.sh
+++ get-new-mirrors.sh
@@ -3,11 +3,11 @@ set -e
 set -u
 
 echo "Finding the fastest Arch mirrors..."
-pacman -Syy pacman-contrib curl --needed --noprogressbar --noconfirm
+pacman -Syy pacman-contrib curl --needed --noprogressbar --noconfirm >/dev/null 2>&1
 curl --silent --get --url https://archlinux.org/mirrorlist/ --data "country=all" --data "use_mirror_status=on" --data "protocol=https" > /tmp/mirrorlist
 sed 's/^#Server/Server/' --in-place /tmp/mirrorlist
 sed '/^##/d' --in-place /tmp/mirrorlist
-rankmirrors --verbose -n 10 --max-time 3 /tmp/mirrorlist > /tmp/fastmirrorlist
+rankmirrors -n 10 --max-time 3 /tmp/mirrorlist > /tmp/fastmirrorlist
 rm /tmp/mirrorlist
 echo "Fast mirror list found. Relocating list."
 mv /tmp/fastmirrorlist /etc/pacman.d/mirrorlist
