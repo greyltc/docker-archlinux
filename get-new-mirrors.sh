4a4f42b517812a19480d92f50b41e9f87c11b5c2
Greyson Christoforo <grey@christoforo.net>
2022-02-13 09:05:20 +0000

    Update get-new-mirrors.sh

--- get-new-mirrors.sh
+++ get-new-mirrors.sh
@@ -2,18 +2,22 @@
 set -e
 set -u
 
-echo "Finding the fastest Arch mirrors..."
-pacman -Syy pacman-contrib curl --needed --noprogressbar --noconfirm >/dev/null 2>&1
+echo "Fetching new list of Arch mirrors..."
+pacman --sync --refresh --refresh --needed --noprogressbar --noconfirm pacman-contrib curl >/dev/null 2>&1
 curl --silent --get --url https://archlinux.org/mirrorlist/ --data "country=all" --data "use_mirror_status=on" --data "protocol=https" > /tmp/mirrorlist
 sed 's/^#Server/Server/' --in-place /tmp/mirrorlist
 sed '/^##/d' --in-place /tmp/mirrorlist
+
+echo "Testing mirror speeds to find the fastest"
 rankmirrors -n 10 --max-time 3 /tmp/mirrorlist > /tmp/fastmirrorlist
 rm /tmp/mirrorlist
-echo "Fast mirror list found. Relocating list."
-mv /tmp/fastmirrorlist /etc/pacman.d/mirrorlist
-echo 'Server = https://mirror.rackspace.com/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
-awk '!seen[$0]++' /etc/pacman.d/mirrorlist
+echo "Fast mirror list found"
+
+echo 'Server = https://mirror.rackspace.com/archlinux/$repo/os/$arch' >> /tmp/fastmirrorlist
+awk '!seen[$0]++' /tmp/fastmirrorlist > /tmp/fastmirrorlist_unique_global
+mv -vf /tmp/fastmirrorlist_unique_global /etc/pacman.d/mirrorlist
 rm -rf /etc/pacman.d/mirrorlist.pacnew
-pacman -Syy
-echo "Mirrorlist updated to be:"
+pacman --sync --refresh --refresh
+
+echo "The mirror list is now:"
 cat /etc/pacman.d/mirrorlist
